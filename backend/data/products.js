fbacbc72de2703aee24c1d130d1991b20f36fe37
mariembarfa <47747730+mariembarfa@users.noreply.github.com>
2022-10-19 21:16:14 +0100

    First commit

---
+++ backend/data/products.js
@@ -0,0 +1,65 @@
+const products = [
+  {
+    name: 'Gilet de sauvetage gonflable Marine',
+    image: '/images/gilet.jpg',
+    description:
+      'Nom de marque:	 Baizhou , Couleur :	 La personnalisation acceptéS ',
+    brand: 'baizhou',
+    category: 'Electronics',
+    price: 220.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+ 
+  {
+    name: 'Matériel de montage E. galvanisé Marine',
+    image: '/images/b.jpg',
+    description:
+      'type de materiel de montage',
+    brand: 'bbbb',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Interrupteur ON - OFF -',
+    image: '/images/Interrupteur ON - OFF - AUTO Dim. 90 x 50 mm (1).jpg',
+    description:
+      'Dim. 90 x 50 mm',
+    brand: 'sony',
+    category: 'Electronics',
+    price: 110.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Pompe Tsunami MKII ATTWOOD',
+    image: '/images/pompe.jpeg',
+    description:
+      'tsun',
+    brand: 'sony',
+    category: 'Electronics',
+    price: 100.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Turbines',
+    image: '/images/tourbine.jpg',
+    description:
+      'Pièces du turbocompresseur Marine roue de turbine les produits à moulage sous vide',
+    brand: 'sony',
+    category: 'Electronics',
+    price: 33.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
